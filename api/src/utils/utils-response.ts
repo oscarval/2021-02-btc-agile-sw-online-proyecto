70bbc9e94218c5a2659277c32228cdfc181f7d7a
oscarval <entulinea.alexis@gmail.com>
2021-11-06 23:24:08 +0100

    add utils response class

---
+++ api/src/utils/utils-response.ts
@@ -0,0 +1,25 @@
+export class UtilsResponse {
+
+  public static responseOK(data: any, message?: string): any {
+    return {
+      code: 0,
+      message: message ? message : "Successful",
+      data: data,
+    };
+  }
+
+  public static responseKO(error?: any): any {
+    return {
+      code: -1,
+      message: error ? error : "There was a problem",
+    };
+  }
+
+  public static responseKOToken(error?: any): any {
+    return {
+      code: -500,
+      message: error ? error : "Token invalid",
+    };
+  }
+}
+
