27fbeef02cf2e0813db29374800f8103a2630cd1
oscarval <entulinea.alexis@gmail.com>
2021-11-07 23:21:00 +0100

    Add functionalityt for shopping cart fix bug update shoppingcart

--- api/src/controllers/shoppingCartController.ts
+++ api/src/controllers/shoppingCartController.ts
@@ -3,16 +3,16 @@ import { UtilsResponse } from "../utils/utils-response";
 
 export class ShoppingCartController {
   public async getAllProductsCart(req: any, res: any): Promise<void> {
-    const userid = req.decoded._id;
-    const cart = await ShoppingCart.findOne({ userid: userid });
+    const userid = req._id;
+    const cart = await ShoppingCart.findOne({ userid });
     res.send(UtilsResponse.responseOK(cart));
   }
 
   public async updateCart(req: any, res: any): Promise<void> {
     try {
-      const userid = req.decoded._id;
+      const userid = req._id;
       let cart = await ShoppingCart.findOneAndUpdate(
-        { userid: userid },
+        { userid },
         req.body,
         {
           new: true,
